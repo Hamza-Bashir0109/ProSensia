a857fb158874f5a2a5e622ec18d3c8f9a27e4485
Hamza-Bashir0109 <167395249+Hamza-Bashir0109@users.noreply.github.com>
2025-06-24 13:15:46 +0500

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    float gpa;
+    cout<<"Enter your name:\n";
+    cin>>name;
+    cout<<"Enter your age:\n";
+    cin>>age;
+    cout<<"Enter your gpa:\n";
+    cin>>gpa;
+
+    cout<<"Your name is "<<name<<"\nYour age is "<<age<<"\nAnd your gpa is "<<gpa;
+}
